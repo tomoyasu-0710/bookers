a348945826fcdd4fe40fece360343854b309882d
tomoyasu-0710 <157125081+tomoyasu-0710@users.noreply.github.com>
2024-02-25 15:41:08 +0000

    「編集]ルーティング、indexビュー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,25 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
-
+  
+  def create
+    @books = Book.new(book_params)
+    if @books.save
+      redirect_to book_path(@books.id)
+    else
+      render :index
+    end
+  end
+  
   def show
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
+  #root to: 'homes#top'
+  get '/top', to: 'homes#top'
   resources :books
- # get '/books' => 'books#index'
+  #get '/books' => 'books#index'
+ # post '/books' => 'books#index'
   #get 'books/show'
   #get 'books/edit'
- # get '/top' => 'homes#top'
+  #get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
