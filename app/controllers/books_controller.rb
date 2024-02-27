f823a28bb9c669d860ea00232f9b1366e74be8b3
tomoyasu-0710 <157125081+tomoyasu-0710@users.noreply.github.com>
2024-02-27 12:24:39 +0000

    「編集機能、削除機能」編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,6 +17,18 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
   private
   def book_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get 'book/:id' => 'books#show', as: 'book'
   get 'book/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'book/:id' => 'books#update', as: 'update_book'
+  delete 'book/:id' => 'books#destroy', as: 'destroy_'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
